8da46f55d5d8423df1aa007cd44da174cf2036d6
Marc Majcher <majcher@gmail.com>
2017-07-21 15:48:50 -0500

    wip

--- stitcher.js
+++ stitcher.js
@@ -31,7 +31,12 @@ function printLadder(ladder) {
       out += seq.dna;
     });
     console.log(out);
+    console.log();
   });
+}
+
+function matchAt(root, seq, pos) {
+  return root.startsWith(transpose(seq.slice(0, root.length - pos)), pos);
 }
 
 function pluck(arr, index) {
@@ -40,37 +45,58 @@ function pluck(arr, index) {
   return [arr[index], newArr];
 }
 
-function findLadders(dna) {
-  for (let i = 0; i < dna.length; i++) {
-    const [root,
-      rest] = pluck(dna, i);
-    const ladder = [
-      [
-        {
-          dna: root,
-          pos: 0
+function buildLadder(data) {
+  for (let i = 0; i < data.remaining.length; i++) {
+    const seq = data.remaining[i];
+    for (let pos = 0; pos < data.currentRoot.length; pos++) {
+      if (matchAt(data.currentRoot, seq, pos)) {
+        data.ladder[data.currentSide].push({ dna: seq, pos });
+        const rest = data.remaining.slice();
+        rest.splice(i, 1);
+
+        if (rest.length === 0) {
+          // We did it!
+          printLadder(data.ladder);
         }
-      ],
-      []
-    ];
-    const side = 1;
-    for (let j = 0; j < rest.length; j++) {
-      const seq = rest[j];
-      for (let pos = 0; pos < root.length; pos++) {
-        if (root.startsWith(transpose(seq.slice(0, root.length - pos)), pos)) {
-          ladder[side] = [
-            {
-              dna: seq,
-              pos
-            }
-          ];
-          printLadder(ladder);
-          console.log();
+        else {
+          // Keep going!
+          const hangover = 0; // calculate hangover - root hangover is +, seq is -
+          if (hangover > 0) {
+            // if there's a hangover on the root side, keep going at pos + seq.length
+          }
+          else if (hangover < 0) {
+            // if there's a hangover on the other side, keep going at
+          }
+          else { // no hangover, boo
+            data.ladder[data.currentSide].pop();
+            return;
+          }
         }
       }
     }
   }
 }
 
-// printLadder(ladder);
+function findLadders(dna) {
+  for (let i = 0; i < dna.length; i++) {
+    const [root, rest] = pluck(dna, i);
+    const data = {
+      ladder: [
+        [
+          {
+            dna: root,
+            pos: 0
+          }
+        ],
+        []
+      ],
+      currentSide: 1,
+      currentPos: 0,
+      currentRoot: root,
+      remaining: rest.slice()
+    };
+    buildLadder(data);
+  }
+}
+
 findLadders(dnaList);
